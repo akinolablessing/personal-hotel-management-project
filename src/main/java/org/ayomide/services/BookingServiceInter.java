daa7e7c93f11ce28d74956f9d5c5addd13bd5066
akinolablessing <akinolablessing1890@gmail.com>
2025-04-11 13:35:06 +0100

    done with services

--- src/main/java/org/ayomide/services/BookingServiceInter.java
+++ src/main/java/org/ayomide/services/BookingServiceInter.java
@@ -1,9 +1,18 @@
 package org.ayomide.services;
 
+import org.ayomide.dto.request.CreateBookingRequest;
+import org.ayomide.dto.request.DeleteBookingRequest;
+import org.ayomide.dto.request.PostBookingRequest;
 import org.ayomide.dto.request.UpdateBookingRequest;
+import org.ayomide.dto.response.CreateBookingResponse;
+import org.ayomide.dto.response.DeleteBookingResponse;
+import org.ayomide.dto.response.PostBookingResponse;
 import org.ayomide.dto.response.UpdateBookingResponse;
 
 public interface BookingServiceInter {
+    CreateBookingResponse createBookingResponse(CreateBookingRequest createBookingRequest);
     UpdateBookingResponse updateBookingResponse(UpdateBookingRequest updateBookingRequest);
-    
+    PostBookingResponse postBookingResponse(PostBookingRequest postBookingRequest);
+    DeleteBookingResponse deleteBookingResponse(DeleteBookingRequest deleteBookingResponse);
+
 }
